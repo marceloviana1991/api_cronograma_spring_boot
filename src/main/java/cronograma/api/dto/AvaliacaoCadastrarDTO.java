e23c769908c30dfac129fd54eb535b5db98be6e4
marcelo <marceloviana1991@gmail.com>
2024-09-22 07:45:30 -0300

    Implementando validacao na classe avaliacao

--- src/main/java/cronograma/api/dto/AvaliacaoCadastrarDTO.java
+++ src/main/java/cronograma/api/dto/AvaliacaoCadastrarDTO.java
@@ -1,8 +1,14 @@
 package cronograma.api.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record AvaliacaoCadastrarDTO(
+        @NotNull
         double nota,
+        @NotBlank
         String texto,
+        @NotNull
         Long eventoId
 ) {
 }
