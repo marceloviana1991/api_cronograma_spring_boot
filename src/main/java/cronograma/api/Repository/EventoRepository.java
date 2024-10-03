9508986d2f4314fdf32535f161038b24e5497fea
marcelo <marceloviana1991@gmail.com>
2024-10-03 18:46:33 -0300

    implementando regras de negócio

--- src/main/java/cronograma/api/Repository/EventoRepository.java
+++ src/main/java/cronograma/api/Repository/EventoRepository.java
@@ -1,9 +1,12 @@
 package cronograma.api.Repository;
 
+import cronograma.api.model.DiaDaSemana;
 import cronograma.api.model.Evento;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalTime;
 import java.util.List;
 
 public interface EventoRepository extends JpaRepository<Evento, Long> {
@@ -13,4 +16,16 @@ public interface EventoRepository extends JpaRepository<Evento, Long> {
     List<Evento> findAllByAtivoTrue(Pageable pageable);
 
     Evento findByIdAndAtivoTrue(Long id);
+
+    @Query("select e from Evento e " +
+            "WHERE (e.horario <= :horario AND e.horarioTermina >= :horario AND e.diaDaSemana = :diaDaSemana) " +
+            "OR (e.horario <= :horarioTermina AND e.horarioTermina >= :horarioTermina AND e.diaDaSemana = :diaDaSemana)")
+    List<Evento> findByChoqueDeHorario(LocalTime horario, LocalTime horarioTermina, DiaDaSemana diaDaSemana);
+
+
+    @Query("select e from Evento e " +
+            "WHERE (e.horario <= :horario AND e.horarioTermina >= :horario AND e.diaDaSemana = :diaDaSemana AND e.id <> :id) " +
+            "OR (e.horario <= :horarioTermina AND e.horarioTermina >= :horarioTermina AND e.diaDaSemana = :diaDaSemana AND e.id <> :id)")
+    List<Evento> findByChoqueDeHorarioAtualizar(LocalTime horario, LocalTime horarioTermina, DiaDaSemana diaDaSemana,
+                                                Long id);
 }
--- src/main/java/cronograma/api/Service/EventoService.java
+++ src/main/java/cronograma/api/Service/EventoService.java
@@ -6,10 +6,13 @@ import cronograma.api.dto.EventoAtualizarDTO;
 import cronograma.api.dto.EventoCadastrarDTO;
 import cronograma.api.infra.ValidacaoException;
 import cronograma.api.model.Cronograma;
+import cronograma.api.model.DiaDaSemana;
 import cronograma.api.model.Evento;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class EventoService {
@@ -24,6 +27,12 @@ public class EventoService {
         var tokenJWT = tokenService.getToken(request);
         var subject = tokenService.getSubject(tokenJWT);
         var cronograma = (Cronograma) cronogramaRepository.findByLogin(subject);
+        var eventosCoqueDeHorario = eventoRepository.findByChoqueDeHorario(eventoCadastrarDTO.horario(),
+                eventoCadastrarDTO.horarioTermina(),
+                DiaDaSemana.fromString(eventoCadastrarDTO.diaDaSemana()));
+        if (!eventosCoqueDeHorario.isEmpty()) {
+            throw new ValidacaoException("Evento com choque de horário");
+        }
         Evento evento = new Evento(eventoCadastrarDTO);
         evento.setCronograma(cronograma);
         return evento;
@@ -33,6 +42,13 @@ public class EventoService {
         var tokenJWT = tokenService.getToken(request);
         var subject = tokenService.getSubject(tokenJWT);
         var cronograma = (Cronograma) cronogramaRepository.findByLogin(subject);
+        var eventosCoqueDeHorario = eventoRepository.findByChoqueDeHorarioAtualizar(eventoAtualizarDTO.horario(),
+                eventoAtualizarDTO.horarioTermina(),
+                eventoAtualizarDTO.diaDaSemana(),
+                eventoAtualizarDTO.id());
+        if (!eventosCoqueDeHorario.isEmpty()) {
+            throw new ValidacaoException("Evento com choque de horário");
+        }
         Evento evento = eventoRepository.getReferenceById(eventoAtualizarDTO.id());
         if (cronograma.equals(evento.getCronograma())) {
             evento.atualizar(eventoAtualizarDTO);
